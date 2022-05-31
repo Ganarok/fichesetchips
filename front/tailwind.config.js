f21cbef85ead2b9ae1e441d7dddc5c69f5a2a8b7
Havane <havane.palabaud@epitech.eu>
2022-05-31 20:33:59 +0200

    Login page without modal

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -7,7 +7,12 @@ module.exports = {
         './nuxt.config.{js,ts}',
     ],
     theme: {
-        extend: {},
+        extend: {
+          colors: {
+            "fiche-green": "#4FEA74",
+            "chips-yellow": "#FFDB57"
+          }
+        },
     },
     plugins: [],
 }
