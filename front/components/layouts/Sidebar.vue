f41c77e3dbae0b9239b6291c942f89b0d3a2beb5
Gabriel Riboldi <sirganarok@gmail.com>
2022-07-25 12:31:42 +0200

    [Feature][FC-137] Glossary, Sidebar Layout

---
+++ front/components/layouts/Sidebar.vue
@@ -0,0 +1,42 @@
+<template>
+    <div name="SidebarLayout" class="flex">
+        <Sidebar />
+
+        <div v-if="topRightPixels" class="absolute top-0 right-0">
+            <img src="../../assets/topRightPixels.svg" alt="pixels" />
+        </div>
+
+        <div
+            class="flex flex-col w-full p-4 z-10 space-y-4 h-screen overflow-auto"
+        >
+            <h1
+                class="font-bold text-xl p-6 self-center sm:self-start sm:text-2xl md:px-8 lg:px-10 lg:text-3xl xl:text-5xl"
+            >
+                {{ title }}
+            </h1>
+
+            <slot />
+        </div>
+    </div>
+</template>
+
+<script>
+import Sidebar from '../subComponent/Sidebar.vue'
+
+export default {
+    name: 'SidebarLayout',
+    props: {
+        title: {
+            type: String,
+            default: 'Title',
+        },
+        topRightPixels: {
+            type: Boolean,
+            default: true,
+        },
+    },
+    components: {
+        Sidebar,
+    },
+}
+</script>
