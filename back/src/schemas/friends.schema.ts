0eb6f9d1342a8ca46670641288f147c3935d26a7
zayfix <zayyfix@gmail.com>
2022-09-08 16:54:47 +0200

    #182 Table friends

---
+++ back/src/schemas/friends.schema.ts
@@ -0,0 +1,24 @@
+import { Table, Column, Model } from 'sequelize-typescript';
+import { UUID, INTEGER, DATE, UUIDV4 } from 'sequelize';
+
+@Table
+export class Friends extends Model {
+  @Column({
+    type: UUID,
+    defaultValue: UUIDV4,
+    primaryKey: true,
+  })
+  id: string;
+
+  @Column({ type: UUID, allowNull: false, unique: true })
+  user_ask_id: string;
+
+  @Column({ type: UUID, allowNull: false })
+  user_answer_id: string;
+
+  @Column({ type: INTEGER, allowNull: false })
+  nbGames: string;
+
+  @Column({ type: DATE, allowNull: false })
+  created_at: string;
+}
