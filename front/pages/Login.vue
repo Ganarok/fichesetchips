b06fc51486f10c17d040f01409ab22cd1dfcd40d
Gabriel Riboldi <g.riboldi@barbablanka.com>
2022-09-15 12:52:45 +0200

    [Feature][FC-143] CustomInput fix, signup/login apiCalls
    [Feature][FC-143] user model for vuex & cors
    
    [Feature][FC-143] ApiCall Register/Login, index store
    
    [Feature][FC-143] SVG fixes

--- front/pages/Login.vue
+++ front/pages/Login.vue
@@ -10,7 +10,7 @@
             class="mt-12 ml-12 max-h-52"
             src="../static/logo.png"
             alt="Fiche&Chips" />
-        <form
+        <div
             action=""
             class="flex flex-col lg:absolute lg:right-32 lg:bottom-32 lg:w-96">
             <div class="ml-5 mb-4">
@@ -22,15 +22,23 @@
                 </p>
             </div>
             <div class="space-y-1">
-                <subComponent-CustomInput placeHolder="Identifiant" />
-                <subComponent-CustomInput placeHolder="Mot de passe" />
+                <CustomInput
+                    :maxLength="36"
+                    @input="(v) => this.handleUsername(v)"
+                    placeHolder="Identifiant" />
+                <CustomInput
+                    :maxLength="64"
+                    @input="(v) => this.handlePassword(v)"
+                    placeHolder="Mot de passe" />
             </div>
             <p class="ml-5 mt-0 underline text-xs opacity-70 cursor-pointer">
                 Mot de passe oublié
             </p>
 
-            <button class="mr-5 self-end text-5xl">Go</button>
-        </form>
+            <button @click="handleLogin" class="mr-5 self-end text-5xl">
+                Go
+            </button>
+        </div>
         <Modal v-show="showModal" @close-modal="showModal = false">
             <subModalSignup
                 v-show="showModal"
@@ -42,14 +50,52 @@
 <script>
 import Vue from 'vue'
 import Modal from '~/components/Modal.vue'
+import CustomInput from '~/components/subComponent/CustomInput.vue'
 import subModalSignup from '~/components/subModals/signup.vue'
+import { apiCall } from '~/utils/apiCall'
+
 export default Vue.extend({
     name: 'Login',
-    components: { Modal, subModalSignup },
+    components: { Modal, subModalSignup, CustomInput },
     data() {
         return {
             showModal: false,
+            username: '',
+            password: '',
         }
+    },
+    methods: {
+        handleUsername(v) {
+            this.username = v
+        },
+        handlePassword(v) {
+            this.password = v
+        },
+        handleLogin() {
+            const { username, password } = this
+
+            if (username && password) {
+                apiCall({
+                    method: 'POST',
+                    route: '/auth/login',
+                    body: JSON.stringify({
+                        username,
+                        password,
+                    }),
+                })
+                    .then((res) => {
+                        this.$store.commit('setUser', {
+                            ...res.user,
+                            access_token: res.access_token,
+                        })
+
+                        this.$router.push('/dashboard')
+                    })
+                    .catch((err) => {
+                        console.log('err', err)
+                    })
+            }
+        },
     },
 })
 </script>
