078dbc28b3b78e4680579b60623f2192625448a1
palegger <pierre-antoine.legger@epitech.eu>
2022-09-15 10:26:06 +0200

    [Feature][FC-147] Add component button

---
+++ front/components/subComponent/Button.vue
@@ -0,0 +1,24 @@
+<template>
+    <button class="border-[3px] rounded-full p-[2%] w-full font-barlow flex justify-center opacity-80 hover:opacity-100" :class="'text-'+color+' '+'border-'+color+' '+'bg-'+filled" @click="callback($event)">
+        {{ buttonText }}
+        <img v-if="image != undefined" :src="image" class="w-[30px] h-[30px] ml-[5px]">
+    </button>
+</template>
+  
+<script>
+    export default {
+        props: {
+            buttonText: String,
+            color: String,
+            filled: String,
+            image: String,
+        },
+        methods: {
+            callback: function (e) {
+            this.$emit("click", e);
+            },
+        },
+    };
+</script>
+
+<!-- <Button buttonText="Exemple" color="fiche-green" v-on:click="test()" filled="chips-yellow" :image="require('../assets/icon.png')"/> -->
