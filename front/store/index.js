aea646b6ca9bb90953c03750be9656195be743ec
palegger <pierre-antoine.legger@epitech.eu>
2022-09-23 12:59:30 +0200

    [Feature][FC-190] Component sidebar connexion and logout

--- front/store/index.js
+++ front/store/index.js
@@ -7,7 +7,7 @@ export const state = () => ({
         role: '',
         createdAt: '',
         updatedAt: '',
-        access_token: '',
+        access_token: 'test',
     },
 })
 
@@ -15,4 +15,23 @@ export const mutations = {
     setUser(state, user) {
         state.user = user
     },
+    logout(state) {
+        state.user.id = ''
+        state.user.username = ''
+        state.user.email = ''
+        state.user.avatar = ''
+        state.user.role = ''
+        state.user.createdAt = ''
+        state.user.updatedAt = ''
+        state.user.access_token = ''
+    }
+}
+
+export const getters = {
+    connected(state) {
+        if(state.user.access_token != '')
+                    return true
+                else
+                    return false
+    }
 }
