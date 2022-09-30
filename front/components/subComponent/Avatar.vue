6e4301b1afa483a5528048b0c59f34c451ebc467
Havane <havane.palabaud@epitech.eu>
2022-09-30 10:01:48 +0200

    [Feature][FC-145] Added profile page and an editable div component
    added alternate profile page
    
    [feature][FC-145] Changed page route from /profile to /user/profile
    
    Badges now work as intended but still wip
    
    Updated badges and added api calls

---
+++ front/components/subComponent/Avatar.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="flex flex-col items-center">
+        <h1
+            v-if="!nick_under"
+            :class="
+                'text-' + nick_size + ' ' + (nick_bold ? 'font-bold' : null)
+            ">
+            {{ nickname }}
+        </h1>
+        <div
+            class="rounded-full bg-fiche-green border-[0.4em] md:border-[0.3em] border-fc-black h-40 md:h-28 my-3"
+            style="aspect-ratio: 1/1"></div>
+        <h1
+            v-if="nick_under"
+            :class="
+                'text-' + nick_size + ' ' + (nick_bold ? 'font-bold' : null)
+            ">
+            {{ nickname }}
+        </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        background: String,
+        border: String,
+        nickname: { type: String, default: null },
+        nick_under: Boolean,
+        nick_size: { type: String, default: '2xl' },
+        nick_bold: Boolean,
+    },
+    methods: {
+        callback: function (e) {
+            this.$emit('click', e)
+        },
+        backgroundColor: function () {
+            if (this.filled == undefined) {
+                return ''
+            } else {
+                return ' bg-' + this.color
+            }
+        },
+        borderColor: function () {
+            if (this.border == undefined) {
+                return ''
+            } else {
+                return ' bg-' + this.color
+            }
+        },
+    },
+}
+</script>
+
+<!-- <Button buttonText="Exemple" color="fiche-green" v-on:click="test()" filled="chips-yellow" :image="require('../assets/icon.png')"/> -->
