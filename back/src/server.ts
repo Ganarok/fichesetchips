a8479858d015c35a9b181397fc308f90a312f30d
mvioleau <manon1.violeau@epitech.eu>
2022-10-03 19:37:32 +0200

    db connection

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,15 +1,14 @@
 import express from "express";
+// import * as cors from 'cors'
 import { routing } from "./routes/routes";
-import connectToDatabase from "./databases/config";
-import 'dotenv/config'
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// app.use(cors())
 
 routing(app);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
-
-const sequelize = connectToDatabase();
