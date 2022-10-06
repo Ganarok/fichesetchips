9dbd6abb0a98ae4b58e646709a63f449d173b2ba
mvioleau <manon1.violeau@epitech.eu>
2022-10-06 10:31:14 +0200

    user model

--- back/src/databases/models/users.ts
+++ back/src/databases/models/users.ts
@@ -1,16 +1,41 @@
-import { DataTypes, Model } from "sequelize";
+import { DATE, ENUM, Model, NOW, STRING, UUID, UUIDV4 } from "sequelize";
 import { sequelize } from '../config'
+import { ROLE } from "../../utils/types/users";
+const bcrypt = require('bcrypt');
 
-class User extends Model { }
+class User extends Model {
+    declare id: string;
+    declare email: string;
+    declare username: string;
+    declare password: string;
+    declare avatar: string;
+    declare role: ROLE;
+    declare preference_id: string;
+    declare last_connection: string;
+}
+
 User.init({
-    firstName: {
-        type: DataTypes.STRING,
-        allowNull: false
+    id: { type: UUID, defaultValue: UUIDV4, primaryKey: true },
+    email: { type: STRING, allowNull: false, unique: true },
+    username: { type: STRING, allowNull: false, unique: true },
+    password: { type: STRING, allowNull: false },
+    avatar: { type: STRING, allowNull: false, defaultValue: "" },
+    role: {
+        allowNull: false, defaultValue: "USER",
+        type: ENUM,
+        values: ["USER", "ADMIN", "SUPERADMIN"],
     },
-    lastName: {
-        type: DataTypes.STRING
-    }
+    preference_id: { type: UUID, allowNull: false, defaultValue: 1 },
+    last_connection: { type: DATE, allowNull: false, defaultValue: NOW }
 }, {
     sequelize,
     modelName: 'User'
+})
+
+User.beforeCreate(async (user: User, options) => {
+    if (user.password) {
+        const salt = await bcrypt.genSalt();
+        const hash = await bcrypt.hash(user.password, salt);
+        user.password = hash
+    }
 })
--- back/src/server.ts
+++ back/src/server.ts
@@ -1,14 +1,25 @@
 import express from "express";
-// import * as cors from 'cors'
+// const cors = require('cors');
 import { routing } from "./routes/routes";
 
 const app = express();
-const port = process.env.PORT || 3000;
+
+function getPort() {
+    if (process.env.PORT) return parseInt(process.env.PORT)
+    else return 3000
+}
+function getHost() {
+    if (process.env.HOST) return process.env.HOST
+    else return "localhost"
+}
+
+const port = getPort()
+const host = getHost()
 
 // app.use(cors())
 
 routing(app);
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+app.listen(port, host, () => {
+    console.log(`App is running ! Go to http://${host}:${port}`);
 });
