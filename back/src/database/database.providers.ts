416b2d0c33242137aeb642cf524d16d6f1436a97
puigs <dev.puigsagur@gmail.com>
2022-10-07 10:37:47 +0200

    modification database provider

--- back/src/database/database.providers.ts
+++ back/src/database/database.providers.ts
@@ -14,7 +14,7 @@ export const databaseProviders = [
         useFactory: async () => {
             const sequelize = new Sequelize({
                 dialect: 'postgres',
-                host: process.env.DB_HOST || '0.0.0.0',
+                host: process.env.DB_HOST || 'localhost',
                 port: parseInt(process.env.DB_PORT) || 5432,
                 username: process.env.DB_USER || 'postgres',
                 password: process.env.DB_PASSWORD || 'postgres',
