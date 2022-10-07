cbbe8c03df502aaf01b9a7d34b5c0928f321b2f2
mvioleau <manon1.violeau@epitech.eu>
2022-10-07 11:31:12 +0200

    Table preferences and references

--- back/src/database/migrations/20221006090847-create-user.js
+++ back/src/database/migrations/20221006090847-create-user.js
@@ -1,26 +1,31 @@
+const { defaultPreference } = require("../seeders/fixtures/preferences")
+const { defaultUser } = require("../seeders/fixtures/users")
+
 module.exports = {
-    up: async(queryInterface, Sequelize) => {
+    up: async(queryInterface, DataTypes) => {
         await queryInterface.createTable('Users', {
-            id: { type: Sequelize.DataTypes.UUID, defaultValue: Sequelize.DataTypes.UUIDV4, primaryKey: true },
-            email: { type: Sequelize.DataTypes.STRING, allowNull: false, unique: true },
-            username: { type: Sequelize.DataTypes.STRING, allowNull: false, unique: true },
-            password: { type: Sequelize.DataTypes.STRING, allowNull: false },
-            avatar: { type: Sequelize.DataTypes.STRING, allowNull: false, defaultValue: "" },
+            id: { type: DataTypes.UUID, defaultValue: DataTypes.UUIDV4, primaryKey: true },
+            email: { type: DataTypes.STRING, allowNull: false, unique: true },
+            username: { type: DataTypes.STRING, allowNull: false, unique: true },
+            password: { type: DataTypes.STRING, allowNull: false },
+            avatar: { type: DataTypes.STRING, allowNull: false, defaultValue: defaultUser.avatar },
             role: {
                 allowNull: false,
-                defaultValue: "USER",
-                type: Sequelize.DataTypes.ENUM,
+                defaultValue: defaultUser.role,
+                type: DataTypes.ENUM,
                 values: ["USER", "ADMIN", "SUPERADMIN"],
             },
-            preference_id: { type: Sequelize.DataTypes.UUID, allowNull: false, defaultValue: "3a9975f8-f34c-4a07-bbff-ab8a9b2e6309" },
-            last_connection: { type: Sequelize.DataTypes.DATE, allowNull: false, defaultValue: Sequelize.DataTypes.NOW },
+            preference_id: { type: DataTypes.UUID, allowNull: false, defaultValue: defaultPreference.id },
+            last_connection: { type: DataTypes.DATE, allowNull: false, defaultValue: DataTypes.NOW },
             createdAt: {
                 allowNull: false,
-                type: Sequelize.DataTypes.DATE
+                type: DataTypes.DATE,
+                defaultValue: DataTypes.NOW
             },
             updatedAt: {
                 allowNull: false,
-                type: Sequelize.DataTypes.DATE
+                type: DataTypes.DATE,
+                defaultValue: DataTypes.NOW
             }
         });
     },
---
+++ back/src/database/seeders/fixtures/users.js
@@ -0,0 +1,40 @@
+const defaultAvatar = require("./avatar.js")
+const { defaultPreference } = require("./preferences")
+module.exports = {
+    defaultUser: {
+        id: "edf1dc34-3534-4cd7-85cf-a9488f1279f9",
+        username: "user",
+        email: "user@email.com",
+        password: "password",
+        avatar: defaultAvatar,
+        role: "USER",
+        preference_id: defaultPreference.id,
+        last_connection: "2022-06-24T11:29:59.619Z",
+        createdAt: "2022-06-24T11:29:59.619Z",
+        updatedAt: "2022-06-24T11:29:59.619Z"
+    },
+    defaultAdmin: {
+        id: "d1ba718d-07c4-41e1-b852-232e686e8535",
+        username: "admin",
+        email: "admin@email.com",
+        password: "password",
+        avatar: defaultAvatar,
+        role: "ADMIN",
+        preference_id: defaultPreference.id,
+        last_connection: "2022-06-24T11:29:59.619Z",
+        createdAt: "2022-06-24T11:29:59.619Z",
+        updatedAt: "2022-06-24T11:29:59.619Z"
+    },
+    defaultSuperAdmin: {
+        id: "abba718d-07c4-41e1-b852-232e686e8535",
+        username: "superadmin",
+        email: "superadmin@email.com",
+        password: "password",
+        avatar: defaultAvatar,
+        role: "SUPERADMIN",
+        preference_id: defaultPreference.id,
+        last_connection: "2022-06-24T11:29:59.619Z",
+        createdAt: "2022-06-24T11:29:59.619Z",
+        updatedAt: "2022-06-24T11:29:59.619Z"
+    }
+}
