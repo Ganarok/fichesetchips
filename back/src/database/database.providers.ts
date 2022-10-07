3cc8be7294f77c24a4d688648dfa983d660cdc4b
puigs <dev.puigsagur@gmail.com>
2022-10-07 10:19:10 +0200

    First version of CD

--- back/src/database/database.providers.ts
+++ back/src/database/database.providers.ts
@@ -14,7 +14,7 @@ export const databaseProviders = [
         useFactory: async () => {
             const sequelize = new Sequelize({
                 dialect: 'postgres',
-                host: process.env.DB_HOST || 'localhost',
+                host: process.env.DB_HOST || '0.0.0.0',
                 port: parseInt(process.env.DB_PORT) || 5432,
                 username: process.env.DB_USER || 'postgres',
                 password: process.env.DB_PASSWORD || 'postgres',
