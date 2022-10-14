18092ec3c55e80748bc58847fcb359259e3ce865
mvioleau <manon1.violeau@epitech.eu>
2022-10-14 17:12:55 +0200

    ✨ Feature: refacto de la nouvelle génération de swagger
    Commit fusion

--- back/src/database/connection.ts
+++ back/src/database/connection.ts
@@ -4,6 +4,7 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 function connectToDatabase(): Sequelize {
+    console.log(process.env.DB_HOST, process.env.DB_PORT, process.env.DB_USER, process.env.DB_PASSWORD, process.env.DB_NAME)
     if (process.env.DB_HOST && process.env.DB_PORT && process.env.DB_USER && process.env.DB_PASSWORD && process.env.DB_NAME) {
 
         const options: Options = { dialect: "postgres", host: process.env.DB_HOST, port: parseInt(process.env.DB_PORT), username: process.env.DB_USER, password: process.env.DB_PASSWORD, database: process.env.DB_NAME }
