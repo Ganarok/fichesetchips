1ae55eb5999edf7eaae6073e97de70e4db6a7531
Puigs <dev.puigsagur@gmail.com>
2022-10-16 17:41:18 +0200

    Add beginning test auth

---
+++ back/src/test/auth/auth.spec.ts
@@ -0,0 +1,31 @@
+import app from '../../server';
+import { expect } from 'chai';
+import {agent as request} from 'supertest';
+
+describe('Auth', () => {
+    it('Login should not return 200 because account do not exist', async () => {
+        const res = await request(app)
+        .post('/auth/login').send({
+            password: '12345678',
+            username: "brayan"
+        });
+        expect(res.status).not.to.equal(200);
+    });
+    it('Register should return 200', async () => {
+        const res = await request(app)
+        .post('/auth/register').send({
+            email: "brayan@gmail.com",
+            password: '12345678',
+            username: "brayan"
+        });
+        expect(res.status).to.equal(200);
+    });
+    it('Login should return 200', async () => {
+        const res = await request(app)
+        .post('/auth/login').send({
+            password: '12345678',
+            username: "brayan"
+        });
+        expect(res.status).to.equal(200);
+    });
+});
