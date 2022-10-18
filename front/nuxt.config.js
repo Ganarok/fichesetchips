5b3d0f6e669f88e2ff0c9cb274a485257510395f
palegger <pierre-antoine.legger@epitech.eu>
2022-10-18 18:43:32 +0200

    [Feature][FC-256] Make vuex persistant

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -45,7 +45,9 @@ export default {
     css: ['@/assets/main.css'],
 
     // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-    plugins: [],
+    plugins: [
+        { src: './plugins/vuex-persist', ssr: false }
+    ],
 
     // Auto import components: https://go.nuxtjs.dev/config-components
     components: true,
