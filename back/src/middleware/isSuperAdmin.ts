d5c1fb637ebedf1145a8fdb4bb1244f9f25cee55
Théo Ackermann <38220718+zayfix@users.noreply.github.com>
2022-10-27 11:22:04 +0200

    🔒️ Middleware isAdmin & isSuperAdmin (#281)
    * 🔒️ Middleware isAdmin & isSuperAdmin
    
    * SuperAdmin autorisé si middleware Admin

---
+++ back/src/middleware/isSuperAdmin.ts
@@ -0,0 +1,31 @@
+import { Response, NextFunction } from "express";
+import * as usersService from "../services/users";
+import { JwtPayload } from "jsonwebtoken";
+import { CustomRequest } from "../middleware/authJwt";
+import { User } from "../database/entities/User";
+
+export const isSuperAdmin = async (
+  req: any,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const user: User | any = await usersService.findProfile(
+      (req as CustomRequest).jwtPayload as JwtPayload
+    );
+
+    if (user == null) {
+      res.status(500).send({ message: "User not found" });
+      return;
+    }
+
+    if (user.user.role != "SUPERADMIN") {
+      res.status(401).send({ message: "Unauthorized" });
+      return;
+    }
+
+    next();
+  } catch (err) {
+    res.status(401).send({ message: "Unauthorized" });
+  }
+};
