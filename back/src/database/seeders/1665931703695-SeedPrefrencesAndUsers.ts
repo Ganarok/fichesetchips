137c2dc9732bfddf5130e961f1ebf4bd67baf06e
mvioleau <manon1.violeau@epitech.eu>
2022-10-28 11:21:30 +0200

    [Feature][FC-181] friends
    get friends
    
    get friends from username
    
    ask a user to be friend
    
    pending friends
    
    delete users after creation for tests
    
    friends tests
    
    remove onetomany from user entity
    
    approve and delete friends

--- back/src/database/seeders/1665931703695-SeedPrefrencesAndUsers.ts
+++ back/src/database/seeders/1665931703695-SeedPrefrencesAndUsers.ts
@@ -1,4 +1,4 @@
-import { getRepository, MigrationInterface, QueryRunner, Repository } from "typeorm";
+import { MigrationInterface, QueryRunner } from "typeorm";
 import { AppDataSource } from "../data-source";
 import { Preference } from "../entities/Preference";
 import { User } from "../entities/User";
@@ -19,14 +19,19 @@ export class SeedPrefrencesAndUsers1665927924682 implements MigrationInterface {
         await PreferenceRepository.save([defaultPreference, darkPreference])
 
         const user = defaultUsers.defaultUser as User
+        const user1 = defaultUsers.defaultUser1 as User
+        const user2 = defaultUsers.defaultUser2 as User
+        const user3 = defaultUsers.defaultUser3 as User
         const admin = defaultUsers.defaultAdmin as User
         const superAdmin = defaultUsers.defaultSuperAdmin as User
-        await UserRepository.save([user, admin, superAdmin])
-
+        await UserRepository.save([user, user1, user2, user3, admin, superAdmin])
 
     }
 
     public async down(_: QueryRunner): Promise<any> {
-        // do nothing
+        const UserRepository = AppDataSource.getRepository(User)
+        const PreferenceRepository = AppDataSource.getRepository(Preference)
+        await UserRepository.delete({})
+        await PreferenceRepository.delete({})
     }
 }
--- back/src/routes/users.ts
+++ back/src/routes/users.ts
@@ -8,27 +8,27 @@ import { getErrorMessage } from "../utils/error-handler/getErrorMessage";
 const router = express.Router();
 
 router.get("/profile", async (req: Request, res) => {
-  /**
-   * @swagger
-   * /users/profile:
-   *   get:
-   *     description: Get my profile.
-   *     tags: 
-   *       - Users
-   *     security:
-   *       - bearerAuth: []
-   *     responses:
-   *       200:
-   *         description: Private profile found.
-   *         content:
-   *           application/json:
-   *             schema: { $ref: '#/definitions/privateProfileResponse' }
-   *       401:
-   *         description: UnAuthorized
-   *         content:
-   *           application/json:
-   *             schema: { $ref: '#/definitions/unAuthorizedResponse' }
-   */
+    /**
+     * @swagger
+     * /users/profile:
+     *   get:
+     *     description: Get my profile.
+     *     tags: 
+     *       - Users
+     *     security:
+     *       - bearerAuth: []
+     *     responses:
+     *       200:
+     *         description: Private profile found.
+     *         content:
+     *           application/json:
+     *             schema: { $ref: '#/definitions/privateProfileResponse' }
+     *       401:
+     *         description: UnAuthorized
+     *         content:
+     *           application/json:
+     *             schema: { $ref: '#/definitions/unAuthorizedResponse' }
+     */
   try {
     const response = await usersService.findProfile(((req as CustomRequest).jwtPayload as JwtPayload).username);
     res.status(200).send({ ...response, message: 'User profile successfully found' });
@@ -38,38 +38,38 @@ router.get("/profile", async (req: Request, res) => {
 })
 
 router.get("/profile/:username", async (req: Request, res) => {
-  /**
-   * @swagger
-   * /users/profile/{username}:
-   *   get:
-   *     description: Get the public profile of a dedicated user.
-   *     tags: 
-   *       - Users
-   *     parameters:
-   *     - in: "path"
-   *       name: "username"
-   *       schema: { type: "string" }
-   *       required: true
-   *       description: "username of the user to get"
-   *     security:
-   *       - bearerAuth: []
-   *     responses:
-   *       200:
-   *         description: Public profile found.
-   *         content:
-   *           application/json:
-   *             schema: { $ref: '#/definitions/publicProfileResponse' }
-   *       401:
-   *         description: User isn't authorized
-   *         content:
-   *           application/json:
-   *             schema: { $ref: '#/definitions/unAuthorizedResponse' }
-   *       404:
-   *         description: Not Found
-   *         content:
-   *           application/json:
-   *             schema: { $ref: '#/definitions/notFoundResponse' }
-   */
+    /**
+     * @swagger
+     * /users/profile/{username}:
+     *   get:
+     *     description: Get the public profile of a dedicated user.
+     *     tags: 
+     *       - Users
+     *     parameters:
+     *     - in: "path"
+     *       name: "username"
+     *       schema: { type: "string" }
+     *       required: true
+     *       description: "username of the user to get"
+     *     security:
+     *       - bearerAuth: []
+     *     responses:
+     *       200:
+     *         description: Public profile found.
+     *         content:
+     *           application/json:
+     *             schema: { $ref: '#/definitions/publicProfileResponse' }
+     *       401:
+     *         description: User isn't authorized
+     *         content:
+     *           application/json:
+     *             schema: { $ref: '#/definitions/unAuthorizedResponse' }
+     *       404:
+     *         description: Not Found
+     *         content:
+     *           application/json:
+     *             schema: { $ref: '#/definitions/notFoundResponse' }
+     */
   try {
     const user_who_asked = await usersService.findProfile(((req as CustomRequest).jwtPayload as JwtPayload).username);
     const user_we_want = await usersService.findProfile(req.params.username);
@@ -93,38 +93,38 @@ router.get("/profile/:username", async (req: Request, res) => {
 })
 
 router.patch("/", async (req: Request, res) => {
-  /**
-   * @swagger
-   * /users:
-   *   patch:
-   *     description: Update my profile.
-   *     tags: 
-   *       - Users
-   *     requestBody:
-   *       description: The updates to do to the User
-   *       required: true
-   *       content:
-   *         application/json:
-   *           schema: { $ref: '#/definitions/updateUserRequest' }
-   *     security:
-   *       - bearerAuth: []
-   *     responses:
-   *       200:
-   *         description: User succesfully updated.
-   *         content:
-   *           application/json:
-   *             schema: { $ref: '#/definitions/privateProfileResponse' }
-   *       401:
-   *         description: User isn't authorized
-   *         content:
-   *           application/json:
-   *             schema: { $ref: '#/definitions/unAuthorizedResponse' }
-   *       404:
-   *         description: Not Found
-   *         content:
-   *           application/json:
-   *             schema: { $ref: '#/definitions/notFoundResponse' }
-   */
+    /**
+     * @swagger
+     * /users:
+     *   patch:
+     *     description: Update my profile.
+     *     tags: 
+     *       - Users
+     *     requestBody:
+     *       description: The updates to do to the User
+     *       required: true
+     *       content:
+     *         application/json:
+     *           schema: { $ref: '#/definitions/updateUserRequest' }
+     *     security:
+     *       - bearerAuth: []
+     *     responses:
+     *       200:
+     *         description: User succesfully updated.
+     *         content:
+     *           application/json:
+     *             schema: { $ref: '#/definitions/privateProfileResponse' }
+     *       401:
+     *         description: User isn't authorized
+     *         content:
+     *           application/json:
+     *             schema: { $ref: '#/definitions/unAuthorizedResponse' }
+     *       404:
+     *         description: Not Found
+     *         content:
+     *           application/json:
+     *             schema: { $ref: '#/definitions/notFoundResponse' }
+     */
   try {
     const response = await usersService.update(((req as CustomRequest).jwtPayload as JwtPayload).username, req.body);
     res.status(200).send({ ...response, message: 'User succesfully updated' });
@@ -134,44 +134,44 @@ router.patch("/", async (req: Request, res) => {
 })
 
 router.patch("/:username", isAdmin, async (req: Request, res) => {
-  /**
-   * @swagger
-   * /users/{username}:
-   *   patch:
-   *     description: Update the profile of a user.
-   *     tags: 
-   *       - Users
-   *     parameters:
-   *     - in: "path"
-   *       name: "username"
-   *       schema: { type: "string" }
-   *       required: true
-   *       description: "username of the user to update"
-   *     requestBody:
-   *       description: The updates to do to the User
-   *       required: true
-   *       content:
-   *         application/json:
-   *           schema: { $ref: '#/definitions/updateUserRequest' }
-   *     security:
-   *       - bearerAuth: []
-   *     responses:
-   *       200:
-   *         description: User succesfully updated.
-   *         content:
-   *           application/json:
-   *             schema: { $ref: '#/definitions/privateProfileResponse' }
-   *       401:
-   *         description: User isn't authorized
-   *         content:
-   *           application/json:
-   *             schema: { $ref: '#/definitions/unAuthorizedResponse' }
-   *       404:
-   *         description: Not Found
-   *         content:
-   *           application/json:
-   *             schema: { $ref: '#/definitions/notFoundResponse' }
-   */
+    /**
+     * @swagger
+     * /users/{username}:
+     *   patch:
+     *     description: Update the profile of a user.
+     *     tags: 
+     *       - Users
+     *     parameters:
+     *     - in: "path"
+     *       name: "username"
+     *       schema: { type: "string" }
+     *       required: true
+     *       description: "username of the user to update"
+     *     requestBody:
+     *       description: The updates to do to the User
+     *       required: true
+     *       content:
+     *         application/json:
+     *           schema: { $ref: '#/definitions/updateUserRequest' }
+     *     security:
+     *       - bearerAuth: []
+     *     responses:
+     *       200:
+     *         description: User succesfully updated.
+     *         content:
+     *           application/json:
+     *             schema: { $ref: '#/definitions/privateProfileResponse' }
+     *       401:
+     *         description: User isn't authorized
+     *         content:
+     *           application/json:
+     *             schema: { $ref: '#/definitions/unAuthorizedResponse' }
+     *       404:
+     *         description: Not Found
+     *         content:
+     *           application/json:
+     *             schema: { $ref: '#/definitions/notFoundResponse' }
+     */
   try {
     const { user } = await usersService.findProfile(req.params.username);
     if (user.role == "SUPERADMIN") {
@@ -194,32 +194,32 @@ router.patch("/:username", isAdmin, async (req: Request, res) => {
 })
 
 router.delete("/", async (req: Request, res) => {
-  /**
-   * @swagger
-   * /users:
-   *   delete:
-   *     description: Delete my profile.
-   *     tags: 
-   *       - Users
-   *     security:
-   *       - bearerAuth: []
-   *     responses:
-   *       200:
-   *         description: User succesfully deleted.
-   *         content:
-   *           application/json:
-   *             schema: { $ref: '#/definitions/privateProfileResponse' }
-   *       401:
-   *         description: User isn't authorized
-   *         content:
-   *           application/json:
-   *             schema: { $ref: '#/definitions/unAuthorizedResponse' }
-   *       404:
-   *         description: Not Found
-   *         content:
-   *           application/json:
-   *             schema: { $ref: '#/definitions/notFoundResponse' }
-   */
+    /**
+     * @swagger
+     * /users:
+     *   delete:
+     *     description: Delete my profile.
+     *     tags: 
+     *       - Users
+     *     security:
+     *       - bearerAuth: []
+     *     responses:
+     *       200:
+     *         description: User succesfully deleted.
+     *         content:
+     *           application/json:
+     *             schema: { $ref: '#/definitions/privateProfileResponse' }
+     *       401:
+     *         description: User isn't authorized
+     *         content:
+     *           application/json:
+     *             schema: { $ref: '#/definitions/unAuthorizedResponse' }
+     *       404:
+     *         description: Not Found
+     *         content:
+     *           application/json:
+     *             schema: { $ref: '#/definitions/notFoundResponse' }
+     */
   try {
     const response = await usersService.destroy(((req as CustomRequest).jwtPayload as JwtPayload).username);
     res.status(200).send({ ...response, message: 'User succesfully deleted' });
@@ -229,38 +229,38 @@ router.delete("/", async (req: Request, res) => {
 })
 
 router.delete("/:username", isAdmin, async (req: Request, res) => {
-  /**
-   * @swagger
-   * /users/{username}:
-   *   delete:
-   *     description: Delete the profile of a user.
-   *     tags: 
-   *       - Users
-   *     parameters:
-   *     - in: "path"
-   *       name: "username"
-   *       schema: { type: "string" }
-   *       required: true
-   *       description: "username of the user to update"
-   *     security:
-   *       - bearerAuth: []
-   *     responses:
-   *       200:
-   *         description: User succesfully deleted.
-   *         content:
-   *           application/json:
-   *             schema: { $ref: '#/definitions/privateProfileResponse' }
-   *       401:
-   *         description: User isn't authorized
-   *         content:
-   *           application/json:
-   *             schema: { $ref: '#/definitions/unAuthorizedResponse' }
-   *       404:
-   *         description: Not Found
-   *         content:
-   *           application/json:
-   *             schema: { $ref: '#/definitions/notFoundResponse' }
-   */
+    /**
+     * @swagger
+     * /users/{username}:
+     *   delete:
+     *     description: Delete the profile of a user.
+     *     tags: 
+     *       - Users
+     *     parameters:
+     *     - in: "path"
+     *       name: "username"
+     *       schema: { type: "string" }
+     *       required: true
+     *       description: "username of the user to update"
+     *     security:
+     *       - bearerAuth: []
+     *     responses:
+     *       200:
+     *         description: User succesfully deleted.
+     *         content:
+     *           application/json:
+     *             schema: { $ref: '#/definitions/privateProfileResponse' }
+     *       401:
+     *         description: User isn't authorized
+     *         content:
+     *           application/json:
+     *             schema: { $ref: '#/definitions/unAuthorizedResponse' }
+     *       404:
+     *         description: Not Found
+     *         content:
+     *           application/json:
+     *             schema: { $ref: '#/definitions/notFoundResponse' }
+     */
   try {
     const { user } = await usersService.findProfile(req.params.username);
     if (user.role == "SUPERADMIN") {
