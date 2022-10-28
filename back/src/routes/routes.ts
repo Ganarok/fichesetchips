202526fde7188f1d7b37e25a92154f03c8dd7191
mvioleau <manon1.violeau@epitech.eu>
2022-10-28 11:21:30 +0200

    [Feature][FC-181] friends
    get friends
    
    get friends from username
    
    ask a user to be friend
    
    pending friends
    
    delete users after creation for tests
    
    friends tests
    
    remove onetomany from user entity
    
    approve and delete friends

--- back/src/routes/routes.ts
+++ back/src/routes/routes.ts
@@ -1,9 +1,11 @@
 import { Express } from "express";
 import users from "./users";
 import auth from "./auth";
+import friends from "./friends";
 import { verifyToken } from "../middleware/authJwt";
 
 export const routing = (app: Express) => {
     app.use("/users", verifyToken, users)
+    app.use("/friends", verifyToken, friends)
     app.use("/auth", auth)
 }
--- back/src/test/auth/auth.spec.ts
+++ back/src/test/auth/auth.spec.ts
@@ -1,116 +1,123 @@
 import app from '../../server';
 import { expect } from 'chai';
-import {agent as request} from 'supertest';
+import { agent as request } from 'supertest';
 import 'mocha'
-let token =  ""
+let token = ""
 
 import defaultUsers from "../../database/fixtures/users"
 import { User } from '../../database/entities/User';
 
 const user = defaultUsers.defaultUser as User
 
+async function delete_user(token: string) {
+    const res = await request(app)
+        .delete(`/users`)
+        .set({ "Authorization": `Bearer ${token}` })
+}
+
 describe('Auth', () => {
     describe('Easy test', () => {
         it('Login should return 404 because account do not exist', async () => {
             const res = await request(app)
-            .post('/auth/login').send({
-                password: "password",
-                username: "account_doesnt_exist"
-            });
+                .post('/auth/login').send({
+                    password: "password",
+                    username: "account_doesnt_exist"
+                });
             expect(res.status).to.equal(404);
         });
         it('Register should return 200', async () => {
             const res = await request(app)
-            .post('/auth/register').send({
-                email: Math.random().toString(36).split(".")[1],
-                password: "password",
-                username: Math.random().toString(36).split(".")[1]
-            });
+                .post('/auth/register').send({
+                    email: Math.random().toString(36).split(".")[1],
+                    password: "password",
+                    username: Math.random().toString(36).split(".")[1]
+                });
             expect(res.status).to.equal(200);
+            await delete_user(res.body.access_token)
         });
         it('Login should return 200', async () => {
             const res = await request(app)
-            .post('/auth/login').send({
-                password: "password",
-                username: user.username
-            });
+                .post('/auth/login').send({
+                    password: "password",
+                    username: user.username
+                });
             expect(res.status).to.equal(200);
             token = res.body.access_token
             const fs = require('fs')
-            fs.writeFile('src/test/.token.txt', token, () => console.error("Error writing file"));  
+            fs.writeFile('src/test/.token.txt', token, () => console.error("Error writing file"));
         });
     });
     describe('Error gestion for register', () => {
         it('Register should return 409 because Query miss email', async () => {
             const res = await request(app)
-            .post('/auth/register').send({
-                password: "password",
-                username: Math.random().toString(36).split(".")[1]
-            });
+                .post('/auth/register').send({
+                    password: "password",
+                    username: Math.random().toString(36).split(".")[1]
+                });
             expect(res.status).to.equal(409);
         });
         it('Register should return 409 because Query miss password', async () => {
             const res = await request(app)
-            .post('/auth/register').send({
-                email: Math.random().toString(36).split(".")[1],
-                username: Math.random().toString(36).split(".")[1]
-            });
+                .post('/auth/register').send({
+                    email: Math.random().toString(36).split(".")[1],
+                    username: Math.random().toString(36).split(".")[1]
+                });
             expect(res.status).to.equal(409);
         });
         it('Register should return 409 because Query miss username', async () => {
             const res = await request(app)
-            .post('/auth/register').send({
-                email: Math.random().toString(36).split(".")[1],
-                password: "password"
-            });
+                .post('/auth/register').send({
+                    email: Math.random().toString(36).split(".")[1],
+                    password: "password"
+                });
             expect(res.status).to.equal(409);
         });
         it('Register should return 400 param not expected', async () => {
             const res = await request(app)
-            .post('/auth/register').send({
-                email: Math.random().toString(36).split(".")[1],
-                password: "password",
-                username: Math.random().toString(36).split(".")[1],
-                banane: "param not expected"
-            });
+                .post('/auth/register').send({
+                    email: Math.random().toString(36).split(".")[1],
+                    password: "password",
+                    username: Math.random().toString(36).split(".")[1],
+                    banane: "param not expected"
+                });
             expect(res.status).to.equal(400);
         });
     });
     describe('Error handler for login', () => {
         it('Login should return 404 because account do not exist', async () => {
             const res = await request(app)
-            .post('/auth/login').send({
-                password: "password",
-                username: "account_doesnt_exist"
-            });
+                .post('/auth/login').send({
+                    password: "password",
+                    username: "account_doesnt_exist"
+                });
             expect(res.status).to.equal(404);
         });
         it('Login should return 404 because password is wrong', async () => {
             const res = await request(app)
-            .post('/auth/login').send({
-                password: "wrong_password",
-                username: user.username
-            });
+                .post('/auth/login').send({
+                    password: "wrong_password",
+                    username: user.username
+                });
             expect(res.status).to.equal(404);
         });
         it('Login should return 400 because param not expected', async () => {
             const res = await request(app)
-            .post('/auth/login').send({
-                password: "password",
-                username: user.username,
-                banane: "param not expected"
-            });
+                .post('/auth/login').send({
+                    password: "password",
+                    username: user.username,
+                    banane: "param not expected"
+                });
             expect(res.status).to.equal(400);
         });
     });
     describe('Test all parameters', () => {
         it('Register should return 200', async () => {
             const res = await request(app)
-            .post('/auth/register').send({
-                email: Math.random().toString(36).split(".")[1],
-                password: "password",
-                username: Math.random().toString(36).split(".")[1]
-            });
+                .post('/auth/register').send({
+                    email: Math.random().toString(36).split(".")[1],
+                    password: "password",
+                    username: Math.random().toString(36).split(".")[1]
+                });
             expect(res.status).to.equal(200);
             expect(res.body.user).to.have.property('email');
             expect(res.body.user).to.have.property('username');
@@ -122,13 +129,14 @@ describe('Auth', () => {
             expect(res.body).to.have.property('access_token');
             expect(res.body).to.have.property('expires_in');
             expect(res.body).to.have.property('message');
+            await delete_user(res.body.access_token)
         });
         it('Login should return 200', async () => {
             const res = await request(app)
-            .post('/auth/login').send({
-                password: "password",
-                username: user.username
-            });
+                .post('/auth/login').send({
+                    password: "password",
+                    username: user.username
+                });
             expect(res.status).to.equal(200);
             expect(res.body.user).to.have.property('email');
             expect(res.body.user).to.have.property('username');
