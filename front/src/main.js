dddf54b858eb7833a1c562301db0a63ec877dc9d
Gabriel Riboldi <g.riboldi@barbablanka.com>
2022-11-10 14:27:15 +0100

    [Hotfix][Front] Fix click-outside directive

--- front/src/main.js
+++ front/src/main.js
@@ -7,9 +7,28 @@ import Store from './store/index.js'
 import 'vue-toastification/dist/index.css'
 import './app.css'
 
-createApp(App)
-    .use(Toast, { timeout: 3500 })
-    .use(Router)
-    .use(CustomI18n)
-    .use(Store)
-    .mount('#app')
+const app = createApp(App)
+
+app.directive('click-outside', {
+    beforeMount (el, binding) {
+        el.clickOutsideEvent = (event) => {
+            // here I check that click was outside the el and his children
+            if (!(el == event.target || el.contains(event.target))) {
+                // and if it did, call method provided in attribute value
+                binding.value(event)
+            }
+        }
+
+        document.body.addEventListener('click', el.clickOutsideEvent)
+    },
+    unmounted(el) {
+        document.body.removeEventListener('click', el.clickOutsideEvent)
+    },
+});
+
+app.use(Toast, { timeout: 3500 })
+app.use(Router)
+app.use(CustomI18n)
+app.use(Store)
+
+app.mount('#app')
