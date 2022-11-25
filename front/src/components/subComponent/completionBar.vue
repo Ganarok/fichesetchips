ecdcb9786f49cd178a6aecd78f89b83c2a259189
palegger <pierre-antoine.legger@epitech.eu>
2022-11-25 14:06:38 +0100

    [Feature][FC-288] Added character creation step system

---
+++ front/src/components/subComponent/completionBar.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class='inline-flex gap-x-4'>
+        <div v-for="n in size">
+            <div class="h-[20px] w-[20px] bg-fc-yellow" :class="{'bg-fc-green': n<= completed}"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        size: Number,
+        completed: {
+            type: Number,
+            default: 1,
+        },
+    },
+}
+</script>
+
+<style>
+.test {
+
+}
+</style>
