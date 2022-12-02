921ea129d66ecf7a86ea654f3d82f704cba96f99
Bagriell <gabriel.buetas@gmail.com>
2022-12-02 14:31:59 +0100

    tilemap editor basics in /front folder instead of /vue folder.

---
+++ front/src/views/phaser/tilemap-editor.vue
@@ -0,0 +1,20 @@
+<script setup>
+import PhaserContainer from "@/components/PhaserContainer";
+</script>
+
+
+<template>
+  <Suspense>
+    <PhaserContainer />
+
+    <template #fallback>
+      <div class="placeholder">Downloading ...</div>
+    </template>
+  </Suspense>
+</template>
+
+<script>
+export default {
+  name: "tilemapEditor",
+};
+</script>
