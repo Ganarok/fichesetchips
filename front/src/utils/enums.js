76c63e1ede6505f92cc824f113b9ac3a58495f2e
Gabriel Riboldi <g.riboldi@barbablanka.com>
2022-12-09 13:43:22 +0100

    [Feature][FC-333] Few Phaser edits
    [Feature][FC-333] Change MapEditor UI (WIP)
    
    [Feature][FC-333] Phaser with Vuex, UI WIP
    
    [Feature][FC-333] Removing RexUI, impoving UI & store communication

--- front/src/utils/enums.js
+++ front/src/utils/enums.js
@@ -22,3 +22,12 @@ export const LANGUAGES = {
     FRENCH: 'Français',
     ENGLISH: 'Anglais',
 }
+
+export const COLORS = {
+    'fc-green': '#4FEA74',
+    'fc-yellow': '#FFDB57',
+    'fc-yellow-trans': '#FFDB57b3',
+    'fc-black': '#1E1E1E',
+    'fc-black-light': '#353535',
+    'fc-red': '#F04E4E',
+}
