68e5a26a482f1e825de90a9ddffbdc72b60067f1
Pierre-Antoine <pierre-antoine.legger@epitech.eu>
2023-03-10 12:13:10 +0100

    [Feature] Responsive character card

--- front/src/components/subComponent/Cards/CharacterCard.vue
+++ front/src/components/subComponent/Cards/CharacterCard.vue
@@ -1,59 +1,31 @@
 <template>
-    <div
-        class="flex flex-col items-center max-w-xs max-h-[550px] border-2 border-fc-green p-2 hover:cursor-pointer"
-    >
-        <router-link
-            :to="`/user/character/${character.id}`"
-        >
-            <div
-                v-if="level || firstname"
-                class="flex items-center justify-between w-full bg-fc-green p-2 text-xl font-bold"
-            >
+    <div class="border-2 border-fc-green p-2 hover:cursor-pointer">
+        <router-link class="h-full w-full flex flex-col items-center" :to="`/user/character/${character.id}`">
+            <div v-if="level || firstname" class="flex items-center justify-between w-full bg-fc-green p-2 text-xl font-bold h-[10%]">
                 <p>
                     {{ `${firstname} ${lastname}` }}
                 </p>
-
-                <p
-                    v-if="level"
-                    class="text-fc-black opacity-60"
-                >
+                <p v-if="level" class="text-fc-black opacity-60">
                     Lvl. {{ level }}
                 </p>
             </div>
 
-            <div
-                v-if="!image"
-                class="relative bg-fc-yellow w-full my-2"
-            >
-                <img
-                    src="@/assets/dragon.svg"
-                    class="object-contain"
-                    :style="grayed ? 'filter: grayscale(1)' : null"
-                >
+            <div class="bg-fc-yellow w-full my-2 h-[66%] flex flex-col justify-end items-center">
+                <div v-if="!image">
+                    <img src="@/assets/dragon.svg" class="object-contain" :style="grayed ? 'filter: grayscale(1)' : null">
+                </div>
+                <div v-else/>
             </div>
-
-            <div
-                v-else
-                class="bg-fc-yellow h-80 w-full m-2"
-            />
-
-            <div class="w-full h-24 bg-fc-green p-2">
-                <p
-                    class="text-sm text-ellipsis text-justify overflow-y-scroll pr-3 break-words h-3/4"
-                    :class="owner || location ? 'h-3/4' : 'h-full'"
-                >
+            
+            <div class="w-full bg-fc-green p-2 flex-1">
+                <p class="text-sm text-ellipsis text-justify overflow-y-scroll pr-3 break-words h-full" :class="owner || location ? 'h-3/4' : 'h-full'">
                     {{ bio }}
                 </p>
-
-                <div
-                    v-if="owner || location"
-                    class="flex flex-row items-center justify-end text-sm space-x-4 w-full h-1/4"
-                >
+                <div v-if="owner || location" class="flex flex-row items-center justify-end text-sm space-x-4 w-full h-full">
                     <p>
                         {{ owner }}
                     </p>
-
-                    <p>
+                    <p> 
                         {{ location }}
                     </p>
                 </div>
