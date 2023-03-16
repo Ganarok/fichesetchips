cf14fca0365c0c30b30945b1b0d6378fe6ba623e
mvioleau <manon1.violeau@epitech.eu>
2023-03-16 14:51:23 +0100

    update seeder

--- back/src/database/fixtures/rooms.ts
+++ back/src/database/fixtures/rooms.ts
@@ -1,14 +1,28 @@
 export default [
     {
+        id: "52a3d2c6-13ed-4759-be26-657fb0c5aa09",
         title: "Room",
         description: "A nice room",
         requirements: "Blabla",
         vocal_url: "blabla",
         is_private: true,
+        is_published: true,
+        password: "password",
+        players_nb_max: 5,
+        // gmId: "edf1dc34-3534-4cd7-85cf-a9488f1279f9",
+        // gameId: "6171aaad-5a3c-4371-b233-b94d8004fcfb"
+    },
+    {
+        id: "4380045a-3d92-433f-b5a9-5b0c3c321a8b",
+        title: "Room",
+        description: "A nice room",
+        requirements: "Blabla",
+        vocal_url: "blabla",
+        is_private: false,
         is_published: false,
         password: "password",
         players_nb_max: 5,
-        gmId: "edf1dc34-3534-4cd7-85cf-a9488f1279f9",
-        gameId: "6171aaad-5a3c-4371-b233-b94d8004fcfb"
+        // gmId: "edf1dc34-3534-4cd7-85cf-a9488f1279f9",
+        // gameId: "30208ae8-95b3-4d3e-aa0f-e0a6c15509b1"
     }
 ]
