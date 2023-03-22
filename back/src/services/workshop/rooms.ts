ff08e0805fb8ff0b35eb7c7411c157b55f620c84
Bagriell <gabriel.buetas@gmail.com>
2023-03-23 00:14:07 +0100

    room service method

--- back/src/services/workshop/rooms.ts
+++ back/src/services/workshop/rooms.ts
@@ -1,3 +1,5 @@
+import { Game } from "../../database/entities/public/Game"
+import { Player } from "../../database/entities/public/Players"
 import { Room } from "../../database/entities/public/Room"
 import { User } from "../../database/entities/public/User"
 import { PublicDataSource } from "../../database/init/datasources/public-data-source"
@@ -5,44 +7,55 @@ import { CreateRoom, UpdateRoom } from "../../utils/types/rooms"
 
 const UserRepository = PublicDataSource.getRepository(User)
 const RoomRepository = PublicDataSource.getRepository(Room)
-
+const PLayerRepository = PublicDataSource.getRepository(Player)
+const GamesRepository = PublicDataSource.getRepository(Game)
 
 export async function findAll(username: string) {
-
-
-    // console.log("findall")
     const user = await UserRepository.findOneByOrFail({ username: username })
     const rooms = await RoomRepository.find({
-        relations: ["gm"],
         where: [
-            { gm: { id: user.id } },
-            { is_private: false, is_published: true }
+            { gm: { id: user.id } }, // rooms where the gm is the user
+            { is_private: false }, // rooms that are not private
+            // { // rooms that are private and have the user as a player
+            //     is_private: true,
+            //     game: {
+            //         players: {
+            //             user: { id: user.id }
+            //         }
+            //     }
+            // }
+        ]
+    });
+
+    const rooms_player_in = await RoomRepository.find({
+        where: [
+            {
+                is_private: true, game: {
+                    players: {
+                        user: { id: user.id }
+                    }
+                }
+            }
         ]
     })
-
-    // // created by me not published
-    // // created by me published
-    // // not created by me public
-    // // not created by me private
-    // // isin
-    // const rooms = await RoomRepository.createQueryBuilder("room").getMany()
-
-    // rooms.filter((room: Room) => {
-    //     room.gm.id == user.id
-    // })
-    // console.log(rooms)
-    return rooms
+    return rooms.concat(rooms_player_in)
 }
+
 export async function findOne(username: string, room_id: string) {
     const user = await UserRepository.findOneByOrFail({ username: username })
-    const rooms = await RoomRepository.find({ where: { id: room_id } })
-    return rooms
+    const rooms_pub = await RoomRepository.findOneByOrFail({ id: room_id, is_private: false })
+    return rooms_pub
 }
+
+// create room and its associate empty game
 export async function create(username: string, room: CreateRoom) {
     const user = await UserRepository.findOneByOrFail({ username: username })
-    const rooms = await RoomRepository.save({ ...room, gm: user })
+    const new_game = await GamesRepository.save(new Game())
+    const rooms = await RoomRepository.save({ ...room, gm: user, game: new_game })
     return rooms
 }
+
+// publish or unplubished depending on query params value
 export async function publish(username: string, body: any, room_id: string) {
     const user = await UserRepository.findOneByOrFail({ username: username })
     const room = await RoomRepository.findOneByOrFail({ id: room_id })
@@ -58,23 +71,23 @@ export async function update(username: string, body: UpdateRoom, room_id: string
         throw new Error("Unexpected parameters")
     }
     const user = await UserRepository.findOneByOrFail({ username: username })
-    console.log(user)
-    console.log(room_id)
-    const room = await RoomRepository.findOneByOrFail({ gm: user, id: room_id })
-    console.log("User:", user)
-    console.log("ROom.gm:", room)
-    let a = await RoomRepository.save({ ...room, ...body })
-    console.log("A: ")
-    console.log(a)
-    return a
+    const room = await RoomRepository.findOneByOrFail({ gm: { id: user.id }, id: room_id })
+    const updated_room = await RoomRepository.save({ ...room, ...body })
+    return updated_room
 }
+
 export async function destroy(username: string, room_id: string) {
-    // const user = await UserRepository.findOneByOrFail({ username: username })
-    // const rooms = await RoomRepository.find({ user_id: user.id })
-    // return rooms
+    const user = await UserRepository.findOneByOrFail({ username: username })
+    const room_to_delete = await RoomRepository.delete({ id: room_id, gm: { id: user.id } })
+    return room_to_delete
 }
+
 export async function join(username: string, room_id: string) {
-    // const user = await UserRepository.findOneByOrFail({ username: username })
-    // const rooms = await RoomRepository.find({ user_id: user.id })
-    // return rooms
+    const user = await UserRepository.findOneByOrFail({ username: username })
+    const room_to_join = await RoomRepository.findOneByOrFail({ id: room_id })
+
+    // links resources
+    const new_player = await PLayerRepository.save({ user: user })
+    room_to_join.game.players.push(new_player)
+    return await RoomRepository.save(room_to_join)
 }
