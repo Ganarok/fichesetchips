34f634ae8770e3c7c1a503bb02fefeed8bf85d76
mvioleau <manon1.violeau@epitech.eu>
2023-04-07 16:51:56 +0200

    begin front

--- front/src/store/modules/room.js
+++ front/src/store/modules/room.js
@@ -13,41 +13,82 @@ export default {
             vocal_url: "",
             isPrivate: false,
         },
-        "user_rooms": [],
-        "public_rooms": [{
-            "id": "52a3d2c6-13ed-4759-be26-657fb0c5aa09",
-            "title": "Room",
-            "description": "A nice room",
-            "requirements": "Blabla",
-            "vocal_url": "blabla",
-            "is_private": true,
-            "is_published": true,
-            "password": "password",
-            "players_nb_max": 5,
-            "created_at": "2023-04-07T13:22:06.245Z",
-            "updated_at": "2023-04-07T13:22:06.245Z",
-            "gm": {
-                "id": "edf1dc34-3534-4cd7-85cf-a9488f1279f9",
-                "email": "user@email.com",
-                "username": "user",
-                "password": "$2b$10$K0oH.iyWccc/O16hKiY13ONt8D6YpN5afheqIE7SMWnh0VG3b7re6",
-                "avatar": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/OOjs_UI_icon_userAvatar.svg/1200px-OOjs_UI_icon_userAvatar.svg.png",
-                "role": "USER",
-                "preference_id": "3a9975f8-f34c-4a07-bbff-ab8a9b2e6309",
-                "last_connection": "2023-04-07T13:22:39.655Z",
-                "created_at": "2022-06-24T11:29:59.619Z",
-                "updated_at": "2023-04-07T13:22:39.660Z"
+        "gm_rooms": [{
+                "id": "52a3d2c6-13ed-4759-be26-657fb0c5aa09",
+                "title": "Room",
+                "description": "A nice room",
+                "requirements": "Blabla",
+                "vocal_url": "blabla",
+                "is_private": true,
+                "is_published": true,
+                "password": "password",
+                "players_nb_max": 5,
+                "created_at": "2023-04-07T13:32:05.534Z",
+                "updated_at": "2023-04-07T13:32:05.534Z",
+                "game": {
+                    "id": "6171aaad-5a3c-4371-b233-b94d8004fcfb",
+                    "status": "planned",
+                    "universe": "cem",
+                    "tilemap": {
+                        "id": "611434bd-8f2a-437d-8547-cc4f2ccd60f7"
+                    },
+                    "story": {
+                        "id": "bb6c9a66-8d75-4077-8994-9d60fe45c33a"
+                    },
+                    "players": [{
+                        "id": "429cf332-c0b9-415a-b6ac-6c4f4ecd4f19",
+                        "state": "not implemented"
+                    }]
+                },
+                "gm": {
+                    "id": "edf1dc34-3534-4cd7-85cf-a9488f1279f9",
+                    "email": "user@email.com",
+                    "username": "user",
+                    "password": "$2b$10$K0oH.iyWccc/O16hKiY13ONt8D6YpN5afheqIE7SMWnh0VG3b7re6",
+                    "avatar": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/OOjs_UI_icon_userAvatar.svg/1200px-OOjs_UI_icon_userAvatar.svg.png",
+                    "role": "USER",
+                    "preference_id": "3a9975f8-f34c-4a07-bbff-ab8a9b2e6309",
+                    "last_connection": "2022-06-24T11:29:59.619Z",
+                    "created_at": "2022-06-24T11:29:59.619Z",
+                    "updated_at": "2022-06-24T11:29:59.619Z"
+                }
             },
-            "game": {
-                "id": "6171aaad-5a3c-4371-b233-b94d8004fcfb",
-                "status": "planned",
-                "universe": "cem",
-                "players": [{
-                    "id": "429cf332-c0b9-415a-b6ac-6c4f4ecd4f19",
-                    "state": "not implemented"
-                }]
+            {
+                "id": "4380045a-3d92-433f-b5a9-5b0c3c321a8b",
+                "title": "Room",
+                "description": "A nice room",
+                "requirements": "Blabla",
+                "vocal_url": "blabla",
+                "is_private": false,
+                "is_published": false,
+                "password": "password",
+                "players_nb_max": 5,
+                "created_at": "2023-04-07T13:32:05.543Z",
+                "updated_at": "2023-04-07T13:32:05.543Z",
+                "game": {
+                    "id": "30208ae8-95b3-4d3e-aa0f-e0a6c15509b1",
+                    "status": "planned",
+                    "universe": "cem",
+                    "tilemap": null,
+                    "story": null,
+                    "players": []
+                },
+                "gm": {
+                    "id": "edf1dc34-3534-4cd7-85cf-a9488f1279f9",
+                    "email": "user@email.com",
+                    "username": "user",
+                    "password": "$2b$10$K0oH.iyWccc/O16hKiY13ONt8D6YpN5afheqIE7SMWnh0VG3b7re6",
+                    "avatar": "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/OOjs_UI_icon_userAvatar.svg/1200px-OOjs_UI_icon_userAvatar.svg.png",
+                    "role": "USER",
+                    "preference_id": "3a9975f8-f34c-4a07-bbff-ab8a9b2e6309",
+                    "last_connection": "2022-06-24T11:29:59.619Z",
+                    "created_at": "2022-06-24T11:29:59.619Z",
+                    "updated_at": "2022-06-24T11:29:59.619Z"
+                }
             }
-        }]
+        ],
+        "published_rooms": [],
+        "player_rooms": []
     },
     mutations: {
         set_room(state, room) {
