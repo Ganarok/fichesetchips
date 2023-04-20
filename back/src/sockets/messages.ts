391fdeff274c083167e7c4b66aed08e84c8c9928
Puigs <47566993+Puigs@users.noreply.github.com>
2023-04-20 11:43:22 +0200

    Feature/websocket (#443)
    * Add units tests before statin developing websocket
    
    * Add socket.io-client for units test
    
    * Update units test with update player position, life and xp
    
    * End of unit tests
    
    * Change message Player... to Character...

--- back/src/sockets/messages.ts
+++ back/src/sockets/messages.ts
@@ -1,10 +1,12 @@
-import { Socket } from "socket.io"
+import { Socket, Namespace } from "socket.io"
 
-export default function messagesSockets(socket: Socket) {
+export default function messagesSockets(namespace: Namespace, socket: Socket) {
     socket.on('message', (n) => {
-        if (n.to)
+        if (n.to) {
             socket.to(n.to).emit('message', n)
-        else
-            socket.broadcast.to(n.roomId).emit('message', n)
+        }
+        else {
+            socket.broadcast.to(n.roomId).emit('message', n.message)
+        }
     })
 }
--- back/src/test/hook.ts
+++ back/src/test/hook.ts
@@ -10,6 +10,7 @@ before(function(done) {
 describe("Root suite", function() {
     import('./auth/auth.spec');
     import('./users/users.spec');
+    import('./websocket/websocket.spec');
     import('./users/friends.spec');
     import('./workshop/stories.spec');
     import('./workshop/maps.spec');
