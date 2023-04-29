11e34a7af71053c537413bd4658abb9acac07ac6
puigs <dev.puigsagur@gmail.com>
2023-04-29 11:59:57 +0200

    Test change ulr http to https

--- front/src/utils/apiCall.js
+++ front/src/utils/apiCall.js
@@ -12,7 +12,7 @@ export async function apiCall({
 }) {
     let baseUrl = "http://localhost:3000"
     if (process.env.NODE_ENV === "production") {
-        baseUrl = `http://${process.env.VUE_APP_SERVER_HOST}:${process.env.VUE_APP_SERVER_PORT}`
+        baseUrl = `https://${process.env.VUE_APP_SERVER_HOST}:${process.env.VUE_APP_SERVER_PORT}`
     }
 
     if (store.state.user.access_token != "") {
